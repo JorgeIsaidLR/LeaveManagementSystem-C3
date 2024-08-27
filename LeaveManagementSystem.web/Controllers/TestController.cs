bec6f2dc85b874b64d3d4383f52254cd60263bb2
nem.isaid <nem.isaid@230199L99807295>
2024-08-26 19:43:50 -0600

    Agregar archivos de proyecto.

---
+++ LeaveManagementSystem.web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using LeaveManagementSystem.web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student of MVC MASTERI",
+                DateOfBird = new DateTime(2001, 04, 23)
+                    
+            };
+            return View(data);
+        }
+    }
+}
